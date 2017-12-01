9c725b393acf8ce2d2e8484897040e5c39680427
Martin STEFFEN <mail@tiph.io>
2017-12-01 18:34:09 +0100

    Updated prototype

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,13 +6,13 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 16:03:30 by msteffen          #+#    #+#             */
-/*   Updated: 2017/11/10 16:50:49 by msteffen         ###   ########.fr       */
+/*   Updated: 2017/12/01 18:33:47 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncat(char *dest, char *src, size_t nb)
+char	*ft_strncat(char *dest, const char *src, size_t nb)
 {
 	unsigned int i;
 	unsigned int y;
