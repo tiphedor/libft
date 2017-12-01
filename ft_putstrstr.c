b7ef1efe11f568deecd6dbf73b881e2865ab24b6
Martin STEFFEN <mail@tiph.io>
2017-12-01 15:45:40 +0100

    temporary commit

--- ft_putstrstr.c
+++ ft_putstrstr.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_putstrstr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/07 17:51:28 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/01 12:35:36 by msteffen         ###   ########.fr       */
+/*   Created: 2017/11/28 17:31:24 by msteffen          #+#    #+#             */
+/*   Updated: 2017/11/28 17:34:31 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <unistd.h>
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	ft_putstrstr(char **str, int len)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((unsigned char *)src)[i];
-		if (((unsigned char *)src)[i] == (unsigned char)c)
-			return (dst + i + 1);
-		i++;
-	}
-	return (NULL);
+	ft_putstrstr_fd(str, len, STDOUT_FILENO);
 }
--- ft_putstrstr_fd.c
+++ ft_putstrstr_fd.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_putstrstr_fd.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/07 17:51:28 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/01 12:35:36 by msteffen         ###   ########.fr       */
+/*   Created: 2017/11/28 17:33:14 by msteffen          #+#    #+#             */
+/*   Updated: 2017/11/28 17:33:37 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	ft_putstrstr_fd(char **str, int len, int fd)
 {
-	unsigned int i;
+	int i;
 
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((unsigned char *)src)[i];
-		if (((unsigned char *)src)[i] == (unsigned char)c)
-			return (dst + i + 1);
-		i++;
-	}
-	return (NULL);
+	i = -1;
+	while (++i < len)
+		ft_putstr_fd(str[i], fd);
 }
