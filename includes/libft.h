3fec8e1fd2b151287d8d2c6fbd4b44b40d47ead5
Martin STEFFEN <mail@tiph.io>
2017-12-01 22:49:36 +0100

    Removed uselss function

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 16:00:07 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/01 19:21:21 by msteffen         ###   ########.fr       */
+/*   Updated: 2017/12/01 22:48:52 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int				ft_memcmp(const void *ss1, const void *ss2, size_t n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memalloc(size_t size);
-void			*ft_memzalloc(size_t size);
 void			ft_memdel(void **ap);
 
 int				ft_atoi(const char *str);
