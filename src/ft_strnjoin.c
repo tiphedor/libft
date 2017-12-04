2de97e37862ccbad78a14b3223af97796ac8d333
Martin STEFFEN <msteffen@student.42.fr>
2017-12-04 16:51:17 +0100

    So that was not working

--- src/ft_strnjoin.c
+++ src/ft_strnjoin.c
@@ -6,7 +6,7 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 18:27:52 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/01 22:29:40 by msteffen         ###   ########.fr       */
+/*   Updated: 2017/12/04 16:50:32 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
 	y = 0;
 	if ((str = ft_strnew(ft_strlen(s1) + n)) == NULL)
 		return (NULL);
-	ft_strcpy(str, s2);
+	ft_strcpy(str, s1);
 	ft_strncat(str, s2, n);
 	return (str);
 }
