ced3b8820c22de79ef849b238179219779dc153b
Martin STEFFEN <msteffen@student.42.fr>
2017-12-08 18:17:32 +0100

    Protected putnchar

--- src/ft_putnchar_fd.c
+++ src/ft_putnchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 18:38:21 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/08 18:10:36 by msteffen         ###   ########.fr       */
+/*   Updated: 2017/12/08 18:17:14 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_putnchar_fd(int fd, int n, char c)
 	int i;
 
 	i = 0;
+	if (n <= 0)
+		return (0);
 	while (i < n)
 	{
 		write(fd, &c, 1);
