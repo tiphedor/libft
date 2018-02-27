5186ddf4c4983d2dcc3c3f5ee4a3932e5182d368
Martin STEFFEN <mail@tiph.io>
2018-02-27 17:05:03 +0100

    Prototype update

--- src/ft_lstdel.c
+++ src/ft_lstdel.c
@@ -6,20 +6,20 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 15:04:45 by msteffen          #+#    #+#             */
-/*   Updated: 2017/11/10 15:06:18 by msteffen         ###   ########.fr       */
+/*   Updated: 2018/02/27 17:03:36 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdel(t_list **alst)
 {
 	t_list *next;
 
 	while (*alst != NULL)
 	{
 		next = (*alst)->next;
-		ft_lstdelone(alst, del);
+		ft_lstdelone(alst);
 		*alst = next;
 	}
 }
--- src/ft_lstdelone.c
+++ src/ft_lstdelone.c
@@ -6,15 +6,14 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 14:59:23 by msteffen          #+#    #+#             */
-/*   Updated: 2017/11/10 15:03:47 by msteffen         ###   ########.fr       */
+/*   Updated: 2018/02/27 17:03:45 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
+void	ft_lstdelone(t_list **alst)
 {
-	del((*alst)->content, (*alst)->content_size);
 	ft_memdel((void**)alst);
 	*alst = NULL;
 }
