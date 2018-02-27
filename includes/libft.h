e550f817ad462425b72993c70b5d6f4a3b21a9cf
Martin STEFFEN <mail@tiph.io>
2018-02-27 16:54:53 +0100

    Added lstpush_back

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 18:28:37 by msteffen          #+#    #+#             */
-/*   Updated: 2018/02/05 15:17:31 by msteffen         ###   ########.fr       */
+/*   Updated: 2018/02/27 16:54:05 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,8 @@ int				ft_abs(int n);
 int				ft_sqrt(int nb);
 
 t_list			*ft_lstnew(void const *content, size_t content_size);
+void			ft_lstpush_back(t_list **lst, void const *content,
+	size_t content_size);
 void			ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
